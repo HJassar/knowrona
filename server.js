f74c6e68a25e62c99950a1812d6fcba95cd4798d
iJassar <jassarhamza@gmail.com>
2020-09-06 10:34:09 -0700

    Great things are brewing! port with env

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/',(req,res,next)=>{
     res.send('Great things are brewing!')
-})
+});
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('connected')
 })
