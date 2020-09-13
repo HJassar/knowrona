cec8db48f4d8ef46f5c2a37062a99cfdfff4869d
iJassar <jassarhamza@gmail.com>
2020-09-13 13:18:19 -0700

    added env functionality

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+// Use dotenv in development
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
