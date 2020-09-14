745fe3ea99599f0d657495284160b2f930de55a0
pranalee123 <pranali123@gmail.com>
2020-09-14 14:43:49 -0500

    initial changes to the footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,14 +6,18 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className="Footer">
-      <ul>
-        <li><Link to='about'>About</Link></li>
-        <li><Link to='contactus'>Contact Us</Link></li>
-        <li><Link to='privacy'>Privacy</Link></li>
-        <li><Link to='termsofuse'>Terms of Use</Link></li>
-        <li>A Project by <img src='./cnsplit_logo_white.png'></img> </li>
-      </ul>
-      <h4>All rights reserved 2020 KnowRona</h4>
+      <nav className="footer-nav">
+        <ul>
+          <li><Link to='about'>About</Link></li>
+          <li><Link to='contactus'>Contact Us</Link></li>
+          <li><Link to='privacy'>Privacy</Link></li>
+          <li><Link to='termsofuse'>Terms of Use</Link></li>
+          <li>A Project by <img src='./cnsplit_logo_white.png'></img> </li>
+        </ul>
+      </nav>
+      <div className="copyrights">
+        <p>All rights reserved 2020 KnowRona</p>
+      </div>
     </div>
   );
 }
