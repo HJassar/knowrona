c2be7fb52269005ab98a16aa6476a680eabfa5ef
Nudzjema <nudzekalo@gmail.com>
2020-09-15 13:55:55 +0200

    add quiz schema

---
+++ models/quiz.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const quizSchema = new mongoose.Schema(
+	{
+		questions : [
+			{
+				id             : {
+					type : mongoose.Schema.Types.ObjectId,
+					ref  : "Question"
+				},
+				points         : Number,
+				selectedChoice : String
+			}
+		],
+		result    : Number
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("Quiz", quizSchema);
