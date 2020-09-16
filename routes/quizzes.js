5f704c448786b7649f9f02d6160c6a6d8e53a5f2
iJassar <jassarhamza@gmail.com>
2020-09-15 18:28:24 -0700

    setting up quiz/generate response

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -1,17 +1,14 @@
-const   express     =   require("express")
-        ,router     =   express.Router()
-        ,Question   =   require('../models/question')
-        ,Quiz       =   require('../models/quiz');
+const express = require("express")
+    , router = express.Router()
+    , Question = require('../models/question')
+    , Quiz = require('../models/quiz');
 
-router.get("/generate", (req,res) => {
+router.get("/generate", (req, res) => {
     Question.aggregate([{ $sample: { size: 5 } }], (err, questions) => {
-        if(err) {
-            console.log("some error");
-        } else {
-            
-            res.json({questions: questions});
-        }
+        if (err) { return console.log(); }
+        
+        res.send('generated quiz')
     });
 })
 
-module.exports  =   router;
+module.exports = router;
