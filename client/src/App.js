b7f32c454dc28fe9bd023f3c71271423eda8a004
iJassar <jassarhamza@gmail.com>
2020-09-17 15:15:51 -0700

    implemented Font Awesome

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import { BrowserRouter as Router, ReactDOM } from 'react-router-dom';
 
 import './App.css';
 
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+
+
+
 import './Header.css';
 
 const Header = () => {
@@ -10,16 +16,16 @@ const Header = () => {
 
   return (
     <header className="Header">
-        {['/', '/mainmenu', '/generatingquiz'].includes(location) ? null : 
-      <div className="Header__logo">
-        <h1>KnowRona</h1>
-      </div>
+      {['/', '/mainmenu', '/generatingquiz'].includes(location) ? null :
+        <div className="Header__logo">
+          <h1>KnowRona</h1>
+        </div>
       }
       <div className="Header__menu">
-      <Link className="Header__menu-button"
-        to="/mainmenu"
+        <Link className="Header__menu-button"
+          to="/mainmenu"
         >
-         Main Menu
+          <FontAwesomeIcon icon={faBars} />
         </Link>
       </div>
     </header>
