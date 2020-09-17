3804a4acf510a21e6594296d0b3436eac2875fb6
David Guzman <davidguzmaninc@gmail.com>
2020-09-16 22:51:36 -0400

    Hides the logo whenever the user is on the landing, main menu, or the generatingquiz page by default

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import './Header.css';
 
 const Header = () => {
+  const [headerLogo, setHeaderLogo] = useState(true);
+
+  const headerLogoStyle = {
+    display: headerLogo ? 'block' : 'none'
+  };
+
+  let location = useLocation().pathname;
+
+  useEffect(() => {
+    if (['/landing', '/mainmenu', '/generatingquiz'].includes(location)) {
+      setHeaderLogo(false);
+    } else {
+      setHeaderLogo(true);
+    }
+  }, []);
+
   return (
     <div className="Header">
-      <div className="Header__logo">
+      <div style={headerLogoStyle} className="Header__logo">
         <h1>KnowRona</h1>
       </div>
       <div className="Header__menu">
-        <Link
-        className="Header__menu-button"
-        to="/mainMenu"
-        >
-         Main Menu
+        <Link className="Header__menu-button" to="/mainMenu">
+          Main Menu
         </Link>
       </div>
     </div>
   );
-}
+};
 
 export default Header;
