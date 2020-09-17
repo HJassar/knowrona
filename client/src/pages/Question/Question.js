2c0f8939ba38fb12843e084a5c5a83c7525b46dc
iJassar <jassarhamza@gmail.com>
2020-09-17 07:52:48 -0700

    style refactoring, dummy setup in questions.js

--- client/src/pages/Question/Question.js
+++ client/src/pages/Question/Question.js
@@ -4,24 +4,19 @@ import './Question.css';
 
 const Question = ({quizIds}) => {
 
-  // return (
-  //   <div className="Question">
-  //     {quizIds.map(question => <p key={question._id}>{question._id}</p>)}
-  //     {/* <p>{stem}</p>
-  //     <p>{choice1}</p>
-  //     <p>{choice2}</p>
-  //     <p>{choice3}</p> */}
-  //   </div>
-  // );
+  const lorem = 'Occaecat sit eiusmod pariatur esse. Et nulla cupidatat ex aliquip non elit dolor tempor nostrud nulla proident. Reprehenderit sit magna do et minim nulla laborum cupidatat cillum. Consequat aute exercitation ipsum occaecat elit eu nisi ea ex mollit id et est. Labore tempor laborum non culpa do est. Est eiusmod excepteur dolor sit occaecat cillum anim occaecat pariatur velit elit aliqua. Dolore dolor ea officia est ipsum cillum.'
+  const [stem,choice1,choice2,choice3] = [lorem,lorem,lorem,lorem]
 
   return (
-    <div className="Question">
-      <p className="Question__question">Occaecat sit eiusmod pariatur esse. Et nulla cupidatat ex aliquip non elit dolor tempor nostrud nulla proident. Reprehenderit sit magna do et minim nulla laborum cupidatat cillum. Consequat aute exercitation ipsum occaecat elit eu nisi ea ex mollit id et est. Labore tempor laborum non culpa do est. Est eiusmod excepteur dolor sit occaecat cillum anim occaecat pariatur velit elit aliqua. Dolore dolor ea officia est ipsum cillum.</p> 
-      <p className="Question__answer">Minim officia voluptate consequat ullamco voluptate Lorem Lorem ad adipisicing sit velit amet.</p> 
-      <p className="Question__answer">Aute ipsum quis reprehenderit quis Lorem qui velit consectetur labore reprehenderit est.</p> 
-      <p className="Question__answer">Laborum qui cillum officia mollit voluptate cillum mollit.</p>   
+    <div className='Question'>
+      {/* {quizIds.map(question => <p key={question._id}>{question._id}</p>)} */}
+      <p className='Question__stem'>{stem}</p>
+      <p className='Question__choice'>{choice1}</p>
+      <p className='Question__choice'>{choice2}</p>
+      <p className='Question__choice'>{choice3}</p>
     </div>
-  )
+  );
+
 }
 
 export default Question;
