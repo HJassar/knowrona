bf930485e68941bb04f046603d1d842d2d589e74
Sneha Gupta <ms.snehagupta2009@gmail>
2020-09-18 21:31:58 +0530

    quiz.js
    Added logic to generate quiz, update Quiz schema and send json to client

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -1,31 +1,63 @@
 const express = require("express");
 const router = express.Router();
-const async = require('async');
+// const async = require('async');
 const Question = require('../models/question');
 const Quiz = require('../models/quiz');
 
 router.get("/generate", (req, res) => {
-    console.log('HITTING THE GENERATE QUIZ ROUTE');
-
-    // Create a quiz
-    // newQuiz = new Quiz;
-
-    // Aggregate the questions
-    Question.aggregate([{ $sample: { size: 5 } }], (err, questions) => {
-        if (err) { return console.log(); }
-        res.json({ questions: questions })
+    console.log('Generate Quiz route accessed');
+    Question.aggregate([{ $sample: { size: 5 } }], (err, foundQuestions) => {
+        if (err) {
+            console.log(err);
+        } else {
+            generateQuiz(req, res, foundQuestions);
+        }
     });
+});
 
-    // push each question into the newly generated quiz
-    // push the question Id's only into quiz.questions
-    // {questions: ['abcd','','','','']}
+const generateQuiz = ((req, res, foundQuestions) => {
+    let questionId;
+    let stem;
+    let choiceId;
+    let choiceText;
+    let choices;
+    let questionsObj;
+    let questions = [];
+    let quizQuestionId = [];
 
-    // res.redirect('/quiz/:quizId/abcd')
+    foundQuestions.forEach(Questionelement => {
+        let quizChoices = [];
 
-})
+        questionId = Questionelement._id;
+        stem = Questionelement.stem;
 
-// /quiz/quizId/result 
+        Questionelement.choices.forEach(choiceElement => {
+            choiceId = choiceElement._id;
+            choiceText = choiceElement.text;
+            choices = { id: choiceId, text: choiceText };
+            quizChoices.push(choices);
+        });
 
+        questionsObj = { id: questionId, stem: stem, choices: quizChoices };
+        quizQuestionId.push({ id: questionId });
+        questions.push(questionsObj);
+    });
+    updateQuizSchema(req, res, quizQuestionId, questions);
+});
 
+const updateQuizSchema = ((req, res, quizQuestionId, questions) => {
+    let quizId;
+    let quiz;
+
+    Quiz.create({ questions: quizQuestionId }, (err, value) => {
+        if (err) {
+            console.log(err);
+        } else {
+            quizId = value._id;
+            quiz = { quizId: quizId, questions: questions };
+            res.json(quiz);
+        }
+    });
+});
 
 module.exports = router;
