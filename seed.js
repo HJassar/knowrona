36a29b9b8bbdc995a601478b10ff01560f5424e8
iJassar <jassarhamza@gmail.com>
2020-09-17 23:16:59 -0700

    fixed the seed, again. Sorry

--- seed.js
+++ seed.js
@@ -82,7 +82,7 @@ const seedDB = async () => {
 		});
 
 		for (const question of questions) {
-			await Question.create({},(err,question)=>{
+			await Question.create(question,(err,question)=>{
 				console.log(question._id+ " has been created");
 			});
 		}
