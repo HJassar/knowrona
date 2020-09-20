18827f7c4bc4fff24812077304c723654fc5892c
iJassar <jassarhamza@gmail.com>
2020-09-19 23:12:07 -0700

    minor refactoring front and back

--- client/src/components/Question/Question.js
+++ client/src/components/Question/Question.js
@@ -1,12 +1,12 @@
-import { faGreaterThanEqual } from "@fortawesome/free-solid-svg-icons";
-import React, { useEffect, useState } from "react";
+import { faGreaterThanEqual } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from 'react';
 
-import "./Question.css";
+import './Question.css';
 
-const Question = ({ quizData, handleChoiceClick, correctChoiceId, questionIndex }) => {
+const Question = ({ quizData, handleChoiceClick, correctChoiceId, questionIndex}) => {
   // const lorem = 'Occaecat sit eiusmod pariatur esse. Et nulla cupidatat ex aliquip non elit dolor tempor nostrud nulla proident. Reprehenderit sit magna do et minim nulla laborum cupidatat cillum. Consequat aute exercitation ipsum occaecat elit eu nisi ea ex mollit id et est. Labore tempor laborum non culpa do est. Est eiusmod excepteur dolor sit occaecat cillum anim occaecat pariatur velit elit aliqua. Dolore dolor ea officia est ipsum cillum.'
   // const [stem,choice1,choice2,choice3] = [lorem,lorem,lorem,lorem]
-  const [selectedChoiceId, setSelectedChoiceId] = useState("");
+  const [selectedChoiceId, setSelectedChoiceId] = useState('');
   let choiceId;
   const handleClick = (e) => {
     // dataset works with lowercase only
@@ -15,76 +15,58 @@ const Question = ({ quizData, handleChoiceClick, correctChoiceId, questionIndex 
     setSelectedChoiceId(choiceId);
     handleChoiceClick(choiceId);
   };
-  // let correctStyle = {
-  //   backgroundColor: "green",
-  // };
-  // let incorrectStyle = {
-  //   backgroundColor: "red",
-  //   textDecoration: "line-through",
-  // };
+  let correctStyle = {
+    backgroundColor: 'green'
+  };
+  let incorrectStyle = {
+    backgroundColor: 'red',
+    textDecoration: 'line-through'
+  };
 
-  document.title = "KnowRona | Question";
+  
   return (
-    <div className="Question">
+    <div className='Question'>
       {!quizData ? (
         <h1>PLEASE GO BACK TO MAIN MENU TO GENERATE A QUIZ</h1>
       ) : (
         <div>
-          <p className="Question__stem">{quizData.questions[questionIndex].stem}</p>
+          <p>{quizData.questions[questionIndex].stem}</p>
           {
             // WILL NEED TO REFACTOR THIS LOGIC INTO DRYER CODE IF ANYONE WANTS TO TAKE A SHOT
             quizData.questions[questionIndex].choices.map((choice) => {
-              return selectedChoiceId ? (
-                <p
-                  // style={correctStyle}
-                  className={`Question__choice ${
-                    correctChoiceId === choice.choiceId
-                      ? "Question__right"
-                      : selectedChoiceId === choice.choiceId
-                      ? "Question__wrong"
-                      : ""
-                  }`}
-                  onClick={handleClick}
-                  key={choice.choiceId}
-                  data-choiceid={choice.choiceId}
-                >
-                  {choice.choiceText}
-                </p>
-              ) : (
-                <p onClick={handleClick} key={choice.choiceId} data-choiceid={choice.choiceId}>
-                  {choice.choiceText}
-                </p>
-              );
-
-              // if (correctChoiceId === choice.choiceId) {
-              //   return (
-              //     <p
-              //       style={correctStyle}
-              //       onClick={handleClick}
-              //       key={choice.choiceId}
-              //       data-choiceid={choice.choiceId}
-              //     >
-              //       {choice.choiceText}
-              //     </p>
-              //   );
-              // } else if (choice.choiceId === selectedChoiceId) {
-              //   return (
-              //     <p
-              //       style={incorrectStyle}
-              //       onClick={handleClick}
-              //       key={choice.choiceId}
-              //       data-choiceid={choice.choiceId}
-              //     >
-              //       {choice.choiceText}
-              //     </p>
-              //   );
-              // } else {
-              //   return (
-              //     <p onClick={handleClick} key={choice.choiceId} data-choiceid={choice.choiceId}>
-              //       {choice.choiceText}
-              //     </p>
-              //   );
-              // }
+              if (correctChoiceId === choice.choiceId) {
+                return (
+                  <p
+                    style={correctStyle}
+                    onClick={handleClick}
+                    key={choice.choiceId}
+                    data-choiceid={choice.choiceId}
+                  >
+                    {choice.choiceText}
+                  </p>
+                );
+              } else if (choice.choiceId === selectedChoiceId) {
+                return (
+                  <p
+                    style={incorrectStyle}
+                    onClick={handleClick}
+                    key={choice.choiceId}
+                    data-choiceid={choice.choiceId}
+                  >
+                    {choice.choiceText}
+                  </p>
+                );
+              } else {
+                return (
+                  <p
+                    onClick={handleClick}
+                    key={choice.choiceId}
+                    data-choiceid={choice.choiceId}
+                  >
+                    {choice.choiceText}
+                  </p>
+                );
+              }
             })
           }
         </div>
