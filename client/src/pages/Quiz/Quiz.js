d11747944926cae40bd239245eccf303f9143534
iJassar <jassarhamza@gmail.com>
2020-09-21 14:03:57 -0700

    redirect back to main menu if /quiz link opened directly

--- client/src/pages/Quiz/Quiz.js
+++ client/src/pages/Quiz/Quiz.js
@@ -6,7 +6,6 @@ import Question from '../../components/Question/Question';
 import NextButton from '../../components/NextButton/NextButton';
 
 const Quiz = ({ quizData, renderPage }) => {
-
   const [correctChoiceId, setCorrectChoiceId] = useState('');
   const [explanationText, setExplanationText] = useState('');
   const [questionIndex, setQuestionIndex] = useState(0);
@@ -33,26 +32,32 @@ const Quiz = ({ quizData, renderPage }) => {
     }
   }
 
+  console.log('quiz data', quizData)
+
   document.title = 'KnowRona | Quiz';
-  return (
-    <div className='Quiz'>
-      <Question
-        quizData={quizData}
-        questionIndex={questionIndex}
-        isAnswered={isAnswered}
-        setIsAnswered={setIsAnswered}
-        disableClick={disableClick}
-        setDisableClick={setDisableClick}
-      />
+  if (quizData) {
+    return (
+      <div className='Quiz'>
+        <Question
+          quizData={quizData}
+          questionIndex={questionIndex}
+          isAnswered={isAnswered}
+          setIsAnswered={setIsAnswered}
+          disableClick={disableClick}
+          setDisableClick={setDisableClick}
+        />
 
-      {isAnswered ? <NextButton
-        handleNextClick={handleNextClick}
-        buttonText={buttonText}
-      />
-      : null}
+        {isAnswered ? <NextButton
+          handleNextClick={handleNextClick}
+          buttonText={buttonText}
+        />
+          : null}
 
-    </div>
-  );
+      </div>
+    );
+  } else{
+    return <Redirect to="/mainmenu" />
+  }
 };
 
 export default Quiz;
