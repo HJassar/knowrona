096ac398e1bf1860acfe443b435ced886300cc53
iJassar <jassarhamza@gmail.com>
2020-09-20 23:54:40 -0700

    minor results page styling

--- client/src/pages/Results/Results.js
+++ client/src/pages/Results/Results.js
@@ -13,7 +13,6 @@ const Results = () => {
 
   return (
     <div className='Results'>
-      <BigLogo />
       <h1><strong>Excellent!</strong></h1>
       <h1>Your score is 96%</h1>
       <button className="primary-btn secondary-btn btn"><FontAwesomeIcon icon={faShareAlt}/><span className="secondary-btn__name">Share Your Score</span></button>
