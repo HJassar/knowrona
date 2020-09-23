60be2926951e200822e060b77600bf4a6b468158
Sneha Gupta <ms.snehagupta2009@gmail>
2020-09-23 07:04:16 +0530

    Fixed the conflict  Please enter the commit message for your changes. Lines starting

--- routes/session.js
+++ routes/session.js
@@ -5,17 +5,16 @@ const Question = require('../models/question');
 
 // Router is /session
 
-// Generate a quiz for session
+// 1. Generate a quiz for session
 router.get('/generate', (req, res) => {
-
-    const currentSession = {
+    const newQuiz = {
         quizId: null,
         questions: []
     }
 
     Quiz.create({}, (err, generatedQuiz) => {
         if (err) return console.log(err)
-        currentSession.quizId = generatedQuiz._id;
+        newQuiz.quizId = generatedQuiz._id;
         const pushingQuestionsToSession = async () => {
             await Question.aggregate([{ $sample: { size: 5 } }], async (err, questions) => {
                 if (err) return console.log(err)
@@ -27,64 +26,75 @@ router.get('/generate', (req, res) => {
                             return { choiceText: choice.text, choiceId: choice._id }
                         })
                     }
-                    currentSession.questions.push(compactQuestion)
+                    newQuiz.questions.push(compactQuestion);
                     generatedQuiz.questions.push(question._id);
-                })
-            })
+                });
+            });
             generatedQuiz.save();
-            // console.log(currentSession)
-            res.send(currentSession)
+            res.send(newQuiz);
         }
         pushingQuestionsToSession();
-    })
-})
+    });
+});
 
 
-// Submit choice
+// 2. Submit choice
 router.get('/:quizId/:questionId/:choiceId/:questionIndex', (req, res) => {
-
     const quizId = req.params.quizId;
     const questionId = req.params.questionId;
     const choiceId = req.params.choiceId;
+    const qi = req.params.questionIndex;
 
     // Compare answer to choices
     Quiz.findById(quizId, (err, currentQuiz) => {
         Question.findById(questionId, (err, question) => {
             if (err) return console.log(err);
 
-            // What do I need to send to the test taker?
+            // Send correct answer to the quiz taker
             const answer = {
                 choices: question.choices.map(c => {
                     return { choiceId: c._id, isCorrect: c.isCorrect }
                 }),
                 explanation: question.explanation
             }
-            res.send(answer)
+            res.send(answer);
 
-            // Manipulate the answered question
-            question.choices.map((c, i) => {
+            // Increment counter of the answered question
+            question.choices.map(c => {
                 if (choiceId == c._id) {
-                    c.counter = c.counter + 1;
+                    c.counter += 1;
                 }
-            })
-            question.save()
+            });
+            question.save();
 
-            // Manipulate the quiz results
-            // const points = currentQuiz.questions[currentIndex].points;
-            // currentQuiz.questions.map and compare with ids
-            // points += 1;
+            //Update selected choice for the Quiz question
+            currentQuiz.questions[qi].selectedChoice = choiceId;
+
+            //Update points for the selected answer
+            question.choices.map(c => {
+                if (choiceId == c._id) {
+                    if (c.isCorrect === true) {
+                        currentQuiz.questions[qi].points += 1;
+                    }
+                }
+            });
+            currentQuiz.save();
+        });
+    });
+});
 
 
-        })
-    })
-})
-
-
+// 3. Compute quiz result and send back
 router.get('/result/:quizId', (req, res) => {
-    console.log(req.params.quizId);
-    const result = 50 + Math.floor(50 * Math.random());
-    console.log(result)
-    res.send({ result: result });
+    const quizId = req.params.quizId;
+    Quiz.findById(quizId, (err, currentQuiz) => {
+        let result = 0;
+        currentQuiz.questions.map(q => {
+            result += q.points;
+            return result;
+        });
+        res.send({ result: result });
+    })
 })
 
 
