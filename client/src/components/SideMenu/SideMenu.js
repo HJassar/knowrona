51b1d5d11c229558baa6b1c771e92568fa0c3366
Paul <webdevpaulmason@gmail.com>
2020-10-19 06:53:58 -0700

    The use of <p> is reserved for paragraphs, which shouldn't be used in the menu. For the X icon we use <i> and the probably the best choice for menu items is using an unordered lists. Also using hr is not recommended any more, the first and second groups should be wrapped in separate divs, and the line is drawn using CSS. Also use font-awesome to add the X.

--- client/src/components/SideMenu/SideMenu.js
+++ client/src/components/SideMenu/SideMenu.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 import ProfileIcon from '../ProfileIcon/ProfileIcon';
 import { connect } from 'react-redux';
 import { toggleProfileHidden } from '../../redux/profile/profile.actions';
@@ -10,17 +13,20 @@ const SideMenu = ( { toggleProfileHidden }) => {
   return (
     <div className='SideMenu'>
       <div className='SideMenu__img'>
-        <p className='SideMenu__img-close' onClick={toggleProfileHidden}>X</p>
+        <FontAwesomeIcon 
+          className='SideMenu__img-close' 
+          onClick={toggleProfileHidden}
+          icon={faTimes} />
         <ProfileIcon />
       </div>
-      <p className='SideMenu__item'>Batman</p>
-      <p className='SideMenu__item'>Edit Profile</p>
-      <p className='SideMenu__item'>LogOut</p>
-      <hr  className='SideMenu__breakline'/>
-      <br/>
-      <Link className='SideMenu__link' to='/home'>Home</Link>
-      <p className='SideMenu__item'>Qbank</p>
-      <p className='SideMenu__item'>Admin Dash</p>
+      <ul>
+        <li className='SideMenu__item'>Batman</li>
+        <li className='SideMenu__item'>Edit Profile</li>
+        <li className='SideMenu__item'>LogOut</li>
+        <li><Link className='SideMenu__link' to='/home'>Home</Link></li>
+        <li className='SideMenu__item'>Qbank</li>
+        <li className='SideMenu__item'>Admin Dash</li>
+      </ul>
     </div>
   );
 }
