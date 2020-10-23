4ff01821c79f9193ba3e38f155601a00b48ba98a
iJassar <jassarhamza@gmail.com>
2020-10-23 07:23:31 -0400

    Frontend: few html fixes in sidemenu

--- client/src/components/SideMenu/SideMenu.js
+++ client/src/components/SideMenu/SideMenu.js
@@ -9,26 +9,30 @@ import { connect } from 'react-redux';
 import { toggleProfileHidden } from '../../redux/profile/profile.actions';
 import './SideMenu.css';
 
-const SideMenu = ( { toggleProfileHidden }) => {
+const SideMenu = ({ toggleProfileHidden }) => {
   return (
     <div className='SideMenu'>
-      <div className='SideMenu__img'>
-        <FontAwesomeIcon 
-          className='SideMenu__img-close' 
-          onClick={toggleProfileHidden}
-          icon={faTimes} />
+      <FontAwesomeIcon
+        className='SideMenu__close'
+        onClick={toggleProfileHidden}
+        icon={faTimes} />
+      
         <ProfileIcon />
+      
+      <div className='SideMenu__section'>
+        <ul className='SideMenu__list'>
+          <li className='SideMenu__item'>Batman</li>
+          <li className='SideMenu__item'>Edit Profile</li>
+          <li className='SideMenu__item'>LogOut</li>
+        </ul>
       </div>
-      <ul className='SideMenu__list'>
-        <div className='SideMenu__section'>
-        <li className='SideMenu__item'>Batman</li>
-        <li className='SideMenu__item'>Edit Profile</li>
-        <li className='SideMenu__item'>LogOut</li>
-        </div>
-        <li className='SideMenu__item'><Link className='SideMenu__link' to='/home'>Home</Link></li>
-        <li className='SideMenu__item'>Qbank</li>
-        <li className='SideMenu__item'>Admin Dash</li>
-      </ul>
+      <div className='SideMenu__section'>
+        <ul className='SideMenu__list'>
+          <li className='SideMenu__item'><Link className='SideMenu__link' to='/home'>Home</Link></li>
+          <li className='SideMenu__item'>Qbank</li>
+          <li className='SideMenu__item'>Admin Dash</li>
+        </ul>
+      </div>
     </div>
   );
 }
