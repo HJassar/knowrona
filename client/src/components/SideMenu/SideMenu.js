cd6472a323618a85f171404d842f714312e9f615
Paul <webdevpaulmason@gmail.com>
2020-10-23 08:23:22 -0700

    (PM) Close menu when an item is selected or user clicks outside of the window

--- client/src/components/SideMenu/SideMenu.js
+++ client/src/components/SideMenu/SideMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,8 +10,31 @@ import { toggleProfileHidden } from '../../redux/profile/profile.actions';
 import './SideMenu.css';
 
 const SideMenu = ({ toggleProfileHidden }) => {
+  // We declare node using the useRef hook in order to make the sidebar div programatically selectable.
+  const node = useRef();
+
+  // Adds a general event listener for a click upon mounting and removes it upon dismounting.
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener("mousedown", handleClick);
+  // return function to be called when unmounted
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
+
+  // The following code detects if the click occured inside the SideMenu. If so, it just returns from the function.  If it was outside, it calls toggleProfileHidden() from the Redux action that has already been setup.
+  const handleClick = e => {
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
+    }
+    // outside click 
+    toggleProfileHidden();
+  }
+
   return (
-    <div className='SideMenu'>
+    <div className='SideMenu' ref={node}>
       <FontAwesomeIcon
         className='SideMenu__close'
         onClick={toggleProfileHidden}
@@ -22,16 +45,16 @@ const SideMenu = ({ toggleProfileHidden }) => {
       <div className='SideMenu__menu' >
         <div className='SideMenu__section'>
           <ul className='SideMenu__list'>
-            <li className='SideMenu__item'>Batman</li>
-            <li className='SideMenu__item'>Edit Profile</li>
-            <li className='SideMenu__item'>LogOut</li>
+            <li className='SideMenu__item' onClick={toggleProfileHidden}>Batman</li>
+            <li className='SideMenu__item' onClick={toggleProfileHidden}>Edit Profile</li>
+            <li className='SideMenu__item' onClick={toggleProfileHidden}>LogOut</li>
           </ul>
         </div>
         <div className='SideMenu__section'>
           <ul className='SideMenu__list'>
-            <li className='SideMenu__item'><Link className='SideMenu__link' to='/home'>Home</Link></li>
-            <li className='SideMenu__item'>Qbank</li>
-            <li className='SideMenu__item'>Admin Dash</li>
+            <li className='SideMenu__item' onClick={toggleProfileHidden}><Link className='SideMenu__link' to='/home'>Home</Link></li>
+            <li className='SideMenu__item' onClick={toggleProfileHidden}>Qbank</li>
+            <li className='SideMenu__item' onClick={toggleProfileHidden}>Admin Dash</li>
           </ul>
         </div>
       </div>
