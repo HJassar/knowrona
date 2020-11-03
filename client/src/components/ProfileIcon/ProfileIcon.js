02e5db17f2017cde9f962bbf5f2fabfc68e2f419
Paul <webdevpaulmason@gmail.com>
2020-11-03 07:15:57 -0800

    (PM)Menu should now load at /home route

--- client/src/components/ProfileIcon/ProfileIcon.js
+++ client/src/components/ProfileIcon/ProfileIcon.js
@@ -1,24 +1,31 @@
-import React from 'react'
+import React from "react";
 
-import './ProfileIcon.css';
-import image from './profile-pic.png';
+import "./ProfileIcon.css";
+import image from "./profile-pic.png";
 
-import { connect } from 'react-redux';
-import { toggleProfileHidden } from '../../redux/profile/profile.actions';
+import { connect } from "react-redux";
+import { toggleProfileHidden } from "../../redux/profile/profile.actions";
 
-const ProfileIcon = ({ }) => {
+const ProfileIcon = ({ avatarSize }) => {
+  let styles = {
+    width: avatarSize,
+  };
   return (
-    <div className='ProfileIcon'>
-      <img className='ProfileIcon-img' src={image} alt='Profile Picture' />
+    <div className="ProfileIcon">
+      <img
+        className="ProfileIcon-img"
+        // style={styles}
+        src={image}
+        alt="Profile Picture"
+      />
     </div>
   );
-}
+};
 
-const mapStateToProps = state => ({
-});
+const mapStateToProps = (state) => ({ avatarSize: state.profile.avatarSize });
 
-const mapDispatchToProps = dispatch => ({
-  toggleProfileHidden: () => dispatch(toggleProfileHidden())
+const mapDispatchToProps = (dispatch) => ({
+  toggleProfileHidden: () => dispatch(toggleProfileHidden()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfileIcon);
--- client/src/redux/profile/profile.actions.js
+++ client/src/redux/profile/profile.actions.js
@@ -1,5 +1,10 @@
-import ProfileActionTypes from './profile.types';
+import { Store } from "express-session";
+import ProfileActionTypes from "./profile.types";
+import store from "../store";
 
+const size = store.getState().profile.avatarSize;
+const avatarSize = size === "50px" ? "100px" : "50px";
 export const toggleProfileHidden = () => ({
-  type: ProfileActionTypes.TOGGLE_PROFILE_HIDDEN
+  type: ProfileActionTypes.TOGGLE_PROFILE_HIDDEN,
+  payload: avatarSize,
 });
